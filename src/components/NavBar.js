8fd2cf067ece654a945aca9018786d77926cf8ba
nej92 <emanunez26@gmail.com>
2021-08-10 20:27:49 -0300

    navBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return  <nav className="NavBarStyle">
+                <h2 className="react">R54CT</h2>                    
+                <div className="itemsNaVBar">
+                    <a href="productos">Productos</a>
+                    <a href="nosotros">Nosotros</a>
+                    <a href="contacto">Contacto</a>
+                </div>
+            </nav>;
+            
+  };
+  
+  export default NavBar;
