070313a1ba9e24d7bf1e8d5c76f61a9120b19aa8
nej92 <emanunez26@gmail.com>
2021-08-18 14:57:30 -0300

    compII

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -19,7 +19,7 @@ const ItemCount = ({stock, initial, onAdd}) => {
 
     return  <div className="btnAgregarAlCarro">
                 <p>{counter}</p>
-                <button onClick={btnRestar} disabled={counter <= 1}>-</button>
+                <button onClick={btnRestar} disabled={counter <= initial}>-</button>
                 <button onClick={onAdd}>Agregar al carrito</button>
                 <button onClick={btnSumar} disabled={counter >= stock}>+</button>
             </div>
